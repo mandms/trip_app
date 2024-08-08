2d766c3ccab35508740e843dd64f49fdc904e0a8
mandms <for4ik45@mail.ru>
2024-08-08 22:13:00 +0300

    Изменил библиотеку классов с .Net Framework на Майкрософт

--- backend/Domain/Interfaces/IEntity.cs
+++ backend/Domain/Interfaces/IEntity.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Interfaces
+﻿namespace Domain.Interfaces
 {
     public interface IEntity
     {
-        long Id { get; set; }
+        public long Id { get; set; }
     }
 }
