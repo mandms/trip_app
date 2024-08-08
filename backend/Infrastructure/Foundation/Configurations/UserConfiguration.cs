387f52681afa91662e8e860bc82cba0d0665dc20
mandms <for4ik45@mail.ru>
2024-08-08 22:42:32 +0300

    Добавил UserConfiguration

---
+++ backend/Infrastructure/Foundation/Configurations/UserConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Domain.Entities;
+
+namespace Infrastructure.Foundation.Configurations
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable(nameof(User));
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.Name)
+                   .HasMaxLength(20)
+                   .IsRequired();
+
+            builder.Property(h => h.Username)
+                   .HasMaxLength(15)
+                   .IsRequired();
+
+            builder.Property(h => h.Biography)
+                   .HasMaxLength(500)
+                   .IsRequired();
+
+            builder.Property(h => h.Email)
+                   .HasMaxLength(320)
+                   .IsRequired();
+
+            builder.Property(h => h.Password)
+                   .HasMaxLength(64)
+                   .IsRequired();
+
+            builder.Property(h => h.Avatar)
+                  .HasMaxLength(150)
+                  .IsRequired();
+        }
+    }
+}
