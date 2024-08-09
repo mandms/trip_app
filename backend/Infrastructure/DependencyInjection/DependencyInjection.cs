fe6caabf66501b4291f05986ce6c47879b0f1fed
mandms <for4ik45@mail.ru>
2024-08-09 11:35:41 +0300

    Добавил DbContext для подключения БД, конфигураций и репозиториев. Добавил DI для подключения сервисов.

---
+++ backend/Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructure.Foundation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static void AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("TripAppDbContext");
+
+            services.AddDbContext<TripAppDbContext>(options =>
+            {
+                options.UseNpgsql(connectionString);
+            });
+            services.InitRepositories();
+        }
+
+        private static void InitRepositories(this IServiceCollection services)
+        { }
+    }
+}
