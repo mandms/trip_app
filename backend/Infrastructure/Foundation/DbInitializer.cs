e499420722718c776b1030ede613c28bc1e06fcf
mandms <for4ik45@mail.ru>
2024-08-13 01:20:57 +0300

    Изменил структуру заполнения данных

--- backend/Infrastructure/Foundation/DbInitializer.cs
+++ backend/Infrastructure/Foundation/DbInitializer.cs
@@ -19,25 +19,31 @@ namespace Infrastructure.Foundation
 
         public void Seed()
         {
+            User john = new User()
+            {
+                Id = 1,
+                Avatar = "avatar1.jpg",
+                Biography = "Biography1..",
+                Email = "one@mail.com",
+                Name = "John",
+                Password = "ddfsdfwfe",
+                Username = "john1"
+            };
+
+            User paul = new User()
+            {
+                Id = 2,
+                Avatar = "avatar2.jpg",
+                Biography = "Biography2..",
+                Email = "two@mail.com",
+                Name = "Paul",
+                Password = "ddfsdfwfe",
+                Username = "paul1"
+            };
+
             modelBuilder.Entity<User>().HasData(
-                   new User() { 
-                       Id = 1, 
-                       Avatar = "avatar1.jpg", 
-                       Biography = "Biography1..", 
-                       Email = "one@mail.com", 
-                       Name="John", 
-                       Password="ddfsdfwfe", 
-                       Username="john1" 
-                   },
-                   new User() { 
-                       Id = 2, 
-                       Avatar = "avatar2.jpg", 
-                       Biography = "Biography2..", 
-                       Email = "two@mail.com", 
-                       Name = "Paul", 
-                       Password = "ddfsdfwfe", 
-                       Username = "paul1" 
-                   }
+                john,   
+                paul  
             );
         }
     }
